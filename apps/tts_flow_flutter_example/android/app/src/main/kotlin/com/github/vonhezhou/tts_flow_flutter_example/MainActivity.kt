80e4bcae1bb4e382723a6a92ad73bf0a5741a37d
von <vonhezhou@gmail.com>
2026-03-26 18:57:08 +0800

    feat: add flutter pacakge and example

---
+++ apps/tts_flow_flutter_example/android/app/src/main/kotlin/com/github/vonhezhou/tts_flow_flutter_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.github.vonhezhou.tts_flow_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ packages/tts_flow_flutter/lib/src/tts_flow_flutter.dart
@@ -0,0 +1,7 @@
+/// {@template tts_flow_flutter}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class TtsFlowFlutter {
+  /// {@macro tts_flow_flutter}
+  const TtsFlowFlutter();
+}
