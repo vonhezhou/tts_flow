6f14976e78cc12a48947fb61c3669ed9b42b11e4
von <vonhezhou@gmail.com>
2026-04-01 11:20:05 +0800

    build: fix missing export

--- packages/tts_flow_flutter/lib/tts_flow_flutter.dart
+++ packages/tts_flow_flutter/lib/tts_flow_flutter.dart
@@ -2,4 +2,6 @@
 library;
 
 export 'src/decoder/decoder_output.dart';
+export 'src/just_audio/chunk_audio_source.dart';
 export 'src/just_audio/just_audio_backend.dart';
+export 'src/just_audio/playback_pos.dart';
